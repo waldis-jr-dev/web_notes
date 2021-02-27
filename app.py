5173c61cf6f142f99b6c5bbb92ee63df1bbc0ceb
Waldis Jr <waldis_jr@outlook.com>
2021-02-27 12:51:46 +0200

    test app

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'lol', 300
+
+
+@app.route('/status/<float:state>')
+def status(state):
+    if state == 'learn':
+        return 'PPC BORING'
+    if state == 'play':
+        return 'VERY VERY GREAT SUPER NICE'
+    else:
+        return 'HZ'
+
+
+if __name__ == '__main__':
+    app.run(port=5123, debug=True)
